4a046ac51dd3e0a64dc5635d68ba3d6aa6bf4647
Benoît Sinivassin <benoit.sinivassin@laas.fr>
2015-06-19 16:33:03 +0200

    remove cycling.py

--- dynamic_graph/rviz/cycling.py
+++
@@ -1,7 +0,0 @@
-from dynamic_graph.sot.core.meta_tasks import generic6dReference, Task, GainAdaptive
-from dynamic_graph.sot.core.matrix_util import matrixToTuple
-from dynamic_graph import plug
-from numpy import *
-
-from dynamic_graph.sot.tools import Oscillator
-
